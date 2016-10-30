e1364223ebdb24dfe7e1180473a32afeb2ca9f40
Bernd <moeller-bernd@gmx.de>
2016-10-30 17:41:18 +0100

    Make the route work with SSL

---
+++ src/main/java/com/moeller/rest/integration/FakeTrustManager.java
@@ -0,0 +1,26 @@
+package com.moeller.rest.integration;
+
+import javax.net.ssl.X509TrustManager;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+
+/**
+ * Created by Bernd on 30.10.2016.
+ *
+ * Serves as a trustmanager which allows all servers to be accessed
+ */
+public class FakeTrustManager implements X509TrustManager {
+    @Override
+    public void checkClientTrusted(X509Certificate[] x509Certificates, String s) throws CertificateException {
+
+    }
+
+    @Override
+    public void checkServerTrusted(X509Certificate[] x509Certificates, String s) throws CertificateException {
+    }
+
+    @Override
+    public X509Certificate[] getAcceptedIssuers() {
+        return null;
+    }
+}
--- src/main/java/com/moeller/rest/integration/MyRouteBuilder.java
+++ src/main/java/com/moeller/rest/integration/MyRouteBuilder.java
@@ -2,6 +2,16 @@ package com.moeller.rest.integration;
 
 import com.moeller.configuration.MyFeatures;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.http.SSLContextParametersSecureProtocolSocketFactory;
+import org.apache.camel.util.jsse.SSLContextClientParameters;
+import org.apache.camel.util.jsse.SSLContextParameters;
+import org.apache.camel.util.jsse.TrustManagersParameters;
+import org.apache.commons.httpclient.protocol.Protocol;
+import org.apache.commons.httpclient.protocol.ProtocolSocketFactory;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLEngine;
+
 
 /**
  * Created by Bernd on 08.10.2016.
@@ -10,8 +20,28 @@ public class MyRouteBuilder extends RouteBuilder{
 
     @Override
     public void configure() throws Exception {
-            from("jetty:http://localhost:8555/triggerit?matchOnUriPrefix=true")
+
+        // configure SSL
+        SSLContextClientParameters sccp = new SSLContextClientParameters();
+
+        SSLContextParameters scp = new SSLContextParameters();
+        TrustManagersParameters tmp = new TrustManagersParameters();
+        tmp.setTrustManager(new FakeTrustManager());
+        scp.setTrustManagers(tmp);
+
+
+        ProtocolSocketFactory factory =
+                new SSLContextParametersSecureProtocolSocketFactory(scp);
+
+        Protocol.registerProtocol("https",
+                new Protocol(
+                        "https",
+                        factory,
+                        443));
+
+
+        from("jetty:http://localhost:8555/triggerit?matchOnUriPrefix=true")
                     .to("log:com.moeller.rest.integration.httpproxy")
-                    .to("http://172.28.128.3:8080/loadb/resources?bridgeEndpoint=true");
+                    .to("https://172.28.128.3:8443/loadb/resources?bridgeEndpoint=true");
     }
 }
